b26278d22ed5ac27bc679d2f79bc9c859ea559ac
Muna-Redi <francischym@gmail.com>
2022-08-23 05:06:19 +0100

    check if a list is a cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a linked list is a cycle
+ * @list: list to be checked
+ * Return: 1 (linked list is a cycle) 0 (not a cycle)
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = NULL, *fast = NULL;
+
+	slow = fast = list;
+	if (slow == NULL)
+		return (0);
+	while (slow && fast && slow->next && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (!slow || !fast)
+			return (0);
+		if (fast->next == slow)
+			return (1); 
+	}
+	return (0);
+}
