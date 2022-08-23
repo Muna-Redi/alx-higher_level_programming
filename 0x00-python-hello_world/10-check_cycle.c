51a104f4c8c6f2e8e6fe7cf74e536e11b9b95b52
Muna-Redi <francischym@gmail.com>
2022-08-23 05:09:43 +0100

    checking a new condition

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,7 +12,7 @@ int check_cycle(listint_t *list)
 	slow = fast = list;
 	if (slow == NULL)
 		return (0);
-	while (slow && fast && slow->next && fast->next)
+	while (slow->next && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
