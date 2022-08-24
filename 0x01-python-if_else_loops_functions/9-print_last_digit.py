d51ea16483534d6faaf4b6a050600f0e6e007641
Muna-Redi <francischym@gmail.com>
2022-08-24 02:42:55 +0100

    negative numbers suited

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,9 +2,11 @@
 
 
 def print_last_digit(number):
-	temp = number
-	if (number < 0):
-		temp = -(number)
-	last_digit = (number) % 10
-	print("{:d}".format(last_digit), end='')
-	return last_digit
+    temp = number
+    if (number < 0):
+    	temp = -(number)
+    last_digit = (number) % 10
+    if (number < 0):
+        last_digit = -(last_digit)
+    print("{:d}".format(last_digit), end='')
+    return last_digit
