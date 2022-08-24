6a4f4120b108be6193dcdb3833d3d0c78157221f
Muna-Redi <francischym@gmail.com>
2022-08-24 03:07:04 +0100

    printing lst digit and defining add function

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,11 +2,8 @@
 
 
 def print_last_digit(number):
-    temp = number
-    if (number < 0):
-    	temp = -(number)
-    last_digit = (number) % 10
-    if (number < 0):
-        last_digit = -(last_digit)
-    print("{:d}".format(last_digit), end='')
-    return last_digit
+	last_digit = abs(number) % 10
+	if (number < 0):
+		last_digit = -(last_digit)
+	print("{:d}".format(last_digit), end='')
+	return digit
