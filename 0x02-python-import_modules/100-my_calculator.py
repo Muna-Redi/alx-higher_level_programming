8125267b41ae3566bbb1218502d731e0a4ee6e9e
Muna-Redi <francischym@gmail.com>
2022-08-26 03:16:20 +0100

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,35 +1,23 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, div, sub, mul
+from sys import argv
+from calculator_1 import add, sub, mul, div
+if __name__ != "__main__":
+    exit()
 
-if __name__ == "__main__":
+argc = len(argv) - 1
+if argc != 3:
+    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
+    exit(1)
+elif argv[2] == '+':
+    result = add(int(argv[1]), int(argv[3]))
+elif argv[2] == '-':
+    result = sub(int(argv[1]), int(argv[3]))
+elif argv[2] == '*':
+    result = mul(int(argv[1]), int(argv[3]))
+elif argv[2] == '/':
+    result = div(int(argv[1]), int(argv[3]))
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
 
-    lenght = len(sys.argv) - 1
-    result = 0
-
-# Incomplete arguments are rejected
-    if lenght != 3:
-        print("usage: {:s} <a> <operator> <b>".format(sys.argv[0]))
-        exit(1)
-
-# Get the arguments and store in variables
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
-
-# Check the type of operation to be done and compute
-    if operator == "+":
-        result = add(a, b)
-    elif operator == "-":
-        result = sub(a, b)
-    elif operator == "*":
-        result = mul(a, b)
-    elif operator == "/":
-        result = div(a, b)
-
-# Unknown operator is rejected
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
-    print("{:d} {} {:d} = {:d}".format(a, sys.argv[2], b, result))
+print("{:s} {:s} {:s} = {:d}".format(argv[1], argv[2], argv[3], result))
