a2d8f95db21b8b9d7e5df4153147aef91f8798c0
Muna-Redi <francischym@gmail.com>
2022-08-29 22:17:56 +0100

    pycodestyle

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    # checking for an empty list
+    if len(my_list) < 1:
+        return (None)
+
+    # innitialise largest to 0
+    largest = 0
+
+    # travarse throught the list in search of the maximu number
+    for num in my_list:
+        if largest < num:
+            largest = num
+
+    # returning the largest number
+    return (largest)
