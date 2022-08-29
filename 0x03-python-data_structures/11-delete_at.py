c9deafba4c7a3467c9f7879427ce8840d288f318
Muna-Redi <francischym@gmail.com>
2022-08-29 23:25:38 +0100

    script script header error

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,11 +1,14 @@
-#!/usr/in/python3
+#!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
     size = len(my_list)
 
+# checks index for negativity or out of range and return list
     if idx < 0 or idx >= size:
         return (my_list)
 
+# deletes index item
     del my_list[idx]
 
+# return list
     return (my_list)
