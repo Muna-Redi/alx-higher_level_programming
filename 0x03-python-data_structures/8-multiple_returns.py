99c64327b4eaf141e3e5dd3bc409657cd95376d1
Muna-Redi <francischym@gmail.com>
2022-08-29 13:47:53 -0700

    multiple returns i.e a tuple

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    lenght = len(sentence)
+    if lenght > 0:
+        char = sentence[0]
+    else:
+        char ="None"
+    my_tuple = (lenght, char)
+    return (my_tuple)
