e8bfe332466e337d65b13b7f3a05356b1f052324
Muna-Redi <francischym@gmail.com>
2022-08-29 11:58:09 -0700

    renamed file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,6 @@
 def print_matrix_integer(matrix=[[]]):
     mat_len = len(matrix)
     for i in range(mat_len):
-        item_len = len(matrix[i])
         for num in matrix[i]:
             print("{:d}".format(num), end = '')
         print()
