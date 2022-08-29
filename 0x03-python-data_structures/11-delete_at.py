25c332637f70e550fd025791e2929d985d817ab9
Muna-Redi <francischym@gmail.com>
2022-08-29 23:22:32 +0100

    index delete

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,12 +3,9 @@
 def delete_at(my_list=[], idx=0):
     size = len(my_list)
 
-    # checks if idx is out of range or negative and return list
     if idx < 0 or idx >= size:
         return (my_list)
 
-    # deleting the item of index idx
     del my_list[idx]
 
-    # return modified list
     return (my_list)
