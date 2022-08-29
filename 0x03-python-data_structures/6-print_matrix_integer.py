d96e24d4bdb2107e0efe07abe03d7d6baae3bce4
Muna-Redi <francischym@gmail.com>
2022-08-29 12:32:37 -0700

    printing matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,11 @@
 def print_matrix_integer(matrix=[[]]):
     mat_len = len(matrix)
     for i in range(mat_len):
+        item_len = len(matrix[i])
+        n = 0
         for num in matrix[i]:
             print("{:d}".format(num), end = '')
-    print()
+            if n != item_len - 1:
+                print(" ", end = '')
+            n += 1
+        print()
