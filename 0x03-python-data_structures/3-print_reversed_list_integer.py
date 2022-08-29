927bd2f393c9605874e14f3a44645689b1e6accf
Muna-Redi <francischym@gmail.com>
2022-08-29 17:50:14 +0100

    used the reverse attribute/method

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,6 @@
 
 def print_reversed_list_integer(my_list=[]):
     list_lenght = len(my_list)
+    my_list.reverse()
     for i in range(list_lenght):
-        print("{:d}".format(my_list[list_lenght - (1 + i)]))
+        print("{:d}".format(my_list[i]))
