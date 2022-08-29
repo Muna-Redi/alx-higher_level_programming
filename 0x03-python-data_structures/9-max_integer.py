b9a012e474720829352c9a0207e12bc764346f2d
Muna-Redi <francischym@gmail.com>
2022-08-29 22:46:50 +0100

    removed trailing white space

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,7 +5,7 @@ def max_integer(my_list=[]):
     if len(my_list) < 1:
         return (None)
 
-    # using the sort method 
+    # using the sort method
     my_list.sort()
 
     # returning the largest number
