6b9be9472bcdc8ed8668361f7ccefdde8061d4ca
Muna-Redi <francischym@gmail.com>
2022-09-01 00:04:25 +0100

    created a fixed size list of size lenth of matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
+    matrix_size = len(matrix)
+    new_matrix = [None] * matrix_size
     j = 0
     for numList in matrix[j]:
-        new_matrix.append(list(map((lambda x: x**2), matrix[j])))
+        new_matrix[j] = (list(map((lambda x: x**2), matrix[j])))
         j += 1
     return (new_matrix)
