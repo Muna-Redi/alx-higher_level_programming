5381bd6dd76873d7a2dd5702b971ee9015c59d9a
Muna-Redi <francischym@gmail.com>
2022-09-13 00:11:06 +0100

    silence skipp

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[indx]), end='')
             count += 1
         except TypeError:
-            continue
+            pass
         except ValueError:
             continue
     print()
