87a08d1a2312ff21822134ecdcd520ee759b8fd7
Muna-Redi <francischym@gmail.com>
2022-09-12 23:49:24 +0100

    typerror exceptionhandled

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,8 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
+        return (True)
     except ValueError:
         return (False)
-    return (True)
+    except TypeError:
+        return (False)
