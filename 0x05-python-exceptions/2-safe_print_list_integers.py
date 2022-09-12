b77f0bc16b7f93b6a9c3ad8f8d80670def060eac
Muna-Redi <francischym@gmail.com>
2022-09-12 23:34:40 +0100

    safe printing

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for indx in range(0, x):
+        try:
+            print("{:d}".format(my_list[indx]), end='')
+            count += 1
+        except TypeError:
+            continue
+        except ValueError:
+            continue
+    print()
+    return (count)
