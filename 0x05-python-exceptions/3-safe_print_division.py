bf5a54c325bc826fe1dacfa48450b983a162a077
Muna-Redi <francischym@gmail.com>
2022-09-13 01:12:19 +0100

    most builtin exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,9 +4,8 @@ def safe_print_division(a, b):
     result = 0
     try:
         result = a / b
-    except ZeroDivisionError:
+    except Exception:
         result = None
-        return None
     finally:
         print("Inside result: {}".format(result))
     return (result)
