a9822d81a8d748ec2fa29d450b55b01270a7a414
Muna-Redi <francischym@gmail.com>
2022-09-13 02:16:04 +0100

    printing division result and raise exception errors

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
 
 def safe_print_division(a, b):
-    result = 0
     try:
-        result = a / b
-    except Exception:
-        result = None
+        res = a / b
+    except:
+        res = None
     finally:
-        print("Inside result: {}".format(result))
-    return (result)
+        print("Inside result: {}".format(res))
+    return res
