3660666469390776cd81c0e82cc6ad982216aae6
Muna-Redi <francischym@gmail.com>
2022-09-21 03:10:07 +0100

    locked classes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,6 +3,5 @@
 class LockedClass:
     """
         locked class that only allows the dynamic instatiation of
-        the attribute first_name
-    """
+        the attribute first_name"""
     __slots__ = ['first_name']
