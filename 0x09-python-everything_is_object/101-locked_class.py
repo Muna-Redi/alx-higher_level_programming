622f1ecb8323c2dd2eb550a626a02209ce6b34eb
Muna-Redi <francischym@gmail.com>
2022-09-22 01:35:12 +0100

    documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 
 class LockedClass:
-    """
-        locked class that only allows the dynamic instatiation of
+    """locked class that only allows the dynamic instatiation of
         the attribute first_name
+
+    Attributes:
+        first_name: only permited dynamically created attribute
     """
     __slots__ = ['first_name']
+    """ the permits only the creation of new instances
+        with attribute first name
+
+    Attributes:
+        first_name: permited dynamically created attribute
+    """
