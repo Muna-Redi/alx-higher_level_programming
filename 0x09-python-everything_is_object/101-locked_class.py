a88ee9bf74e85beb428c98ea341ef1c438698a53
Muna-Redi <francischym@gmail.com>
2022-09-22 01:21:09 +0100

    ommited script header

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 class LockedClass:
     """
         locked class that only allows the dynamic instatiation of
-        the attribute first_name"""
+        the attribute first_name
+    """
     __slots__ = ['first_name']
