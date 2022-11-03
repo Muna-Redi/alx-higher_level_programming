4e14a6fde5e1dbef11894fa747d1bcaf946b18c6
Muna-Redi <francischym@gmail.com>
2022-11-03 21:40:00 +0100

    listing privileges

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- List all privileges of users user_0d_1 and user_0d_2 on server localhost
+-- Privileges for user_0d_1
+SHOW GRANTS FOR "user_0d_1"@"localhost";
+-- Privileges for user_0d_2
+SHOW GRANTS FOR "user_0d_2"@"localhost";
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+MYSQL user creation, Granting privileges, queries
