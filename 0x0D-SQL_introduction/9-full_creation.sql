375d00b77d252ce638595f77f97e1d060b523ab9
Muna-Redi <francischym@gmail.com>
2022-11-03 05:59:55 +0100

    values

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,5 +6,10 @@ CREATE TABLE IF NOT EXISTS second_table (
 	score integer
 );
 -- adding multipler rows
-INSERT INTO second_table (id, name, score)
-VALUES (1, "John", 10), (2, "ALEX", 3), (3, "Bob", 14), (4, "George", 8);
+INSERT INTO second_table
+VALUES (1, "John", 10);
+INSERT INTO second_table
+VALUES(2, "ALEX", 3);
+INSERT INTO second_table
+VALUES(3, "Bob", 14);
+VALUES (4, "George", 8);
