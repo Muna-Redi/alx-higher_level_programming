a371946366aafcf310344915aa8e2d7bae3503e0
Muna-Redi <francischym@gmail.com>
2023-01-18 02:51:36 +0100

    post

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    Http requests POST
+"""
+from requests import post
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = {'email': argv[2]}
+    response = post(url, data=email)
+    print(response.text)
