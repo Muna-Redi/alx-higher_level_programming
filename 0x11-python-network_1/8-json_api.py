4ea3dc8855cf283f11ff96dfcfcb63f14f9d0c1d
Muna-Redi <francischym@gmail.com>
2023-01-18 03:27:09 +0100

    json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    http json
+"""
+from requests import post
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(argv) > 1:
+        q = {'q': argv[1]}
+    else:
+        q = {'q': ''}
+    response = post(url, data=q)
+    try:
+        obj = response.json()
+        if len(obj) == 0:
+            print('No result')
+        else:
+            print('[{}] {}'.format(obj['id'], obj['name']))
+    except:
+        print('Not a valid JSON')
